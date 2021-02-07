583b9bfc7b3a01a4d4a59f58a859422f9dc836a1
Alli Neuman <alli.neuman@gmail.com>
2021-02-07 12:47:07 -0600

    add props to dynamically add notes from js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-//1. Create a new React app.
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
+import App from "./components/App";
 
-
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
